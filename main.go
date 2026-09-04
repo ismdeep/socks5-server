8551b7a9b51c7bcaeffdff9f6849621b620d839b
L. Jiang <l.jiang.1024@gmail.com>
2026-09-04 15:57:47 +0800

    feat: add startup banner and improve server startup logging

--- main.go
+++ main.go
@@ -1,29 +1,43 @@
 package main
 
 import (
+	_ "embed"
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/armon/go-socks5"
 
 	"github.com/ismdeep/socks5-server/conf"
 )
 
+//go:embed banner.txt
+var banner string
+
 func main() {
+
+	fmt.Println(banner)
+
 	cfg := &socks5.Config{}
 
 	if conf.ROOT.Username != "" {
+		log.Println("authentication enabled")
 		cfg.Credentials = socks5.StaticCredentials{
 			conf.ROOT.Username: conf.ROOT.Password,
 		}
+	} else {
+		log.Println("WARN!!! authentication disabled")
 	}
 
 	server, err := socks5.New(cfg)
 	if err != nil {
-		panic(err)
+		_, _ = fmt.Fprintf(os.Stderr, "failed to create server: %v\n", err)
+		os.Exit(1)
 	}
 
-	log.Printf("BIND: %v\n", conf.ROOT.Bind)
+	log.Printf("socks5 server started, bind: %v\n", conf.ROOT.Bind)
 	if err := server.ListenAndServe("tcp", conf.ROOT.Bind); err != nil {
-		panic(err)
+		_, _ = fmt.Fprintf(os.Stderr, "failed to start server: %v\n", err)
+		os.Exit(1)
 	}
 }
